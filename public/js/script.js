0b30934ebc7b8c8ba73b3dff15d886fbbad126e8
Eyesofbanquo <mls103020@utdallas.edu>
2016-05-10 15:07:03 -0500

    Adding some logo changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,32 @@
 $(function(){
 	$(".rslides").responsiveSlides();
+
+	$("#isLive").click(function(){
+		$("#isLive").html("Let's go!");
+	});
 		
+	(function poll() {
+		$.ajax({
+		url: "https://api.twitch.tv/kraken/streams/bum1six3",
+		type: "GET",
+		success: function(data){
+			if (data["stream"] == null) {
+				$("#liveId").html("offline");
+			} else {
+				$("#liveId").html("live");
+				$("#liveId").click(function(){
+					window.location = "http://www.twitch.tv/bum1six3/embed";
+				});
+			}
+			console.log("polling");
+		},
+		dataType: "json",
+		complete: setTimeout(function() {poll()}, 18000000),
+		timeout: 9000000
+	})
+	})();
+	
+
 });
+
+
