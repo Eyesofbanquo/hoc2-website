0b84fd5ca6ee8640a22076d3f3aa303f752f3787
Eyesofbanquo <mls103020@utdallas.edu>
2016-05-10 16:26:09 -0500

    removing rest call in myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -4,13 +4,7 @@ require 'slim/include'
 require 'rest-client'
 
 #Check to see if the stream is online. Use this information to update the button on main page
-isLive = (JSON.parse RestClient.get "https://api.twitch.tv/kraken/streams/bum1six3")["stream"]
-isLiveClass = "offline"
-if isLive === nil
-	isLiveClass = "offline"
-else
-	isLiveClass = "online"
-end
+
 
 class HOC<Sinatra::Base
 	set :static, true
