c03e054e9fbf44f0f2e74383a0b34bc2f5cc65e7
Eyesofbanquo <mls103020@utdallas.edu>
2016-05-10 15:12:30 -0500

    removed the polling

--- public/js/script.js
+++ public/js/script.js
@@ -5,26 +5,7 @@ $(function(){
 		$("#isLive").html("Let's go!");
 	});
 		
-	(function poll() {
-		$.ajax({
-		url: "https://api.twitch.tv/kraken/streams/bum1six3",
-		type: "GET",
-		success: function(data){
-			if (data["stream"] == null) {
-				$("#liveId").html("offline");
-			} else {
-				$("#liveId").html("live");
-				$("#liveId").click(function(){
-					window.location = "http://www.twitch.tv/bum1six3/embed";
-				});
-			}
-			console.log("polling");
-		},
-		dataType: "json",
-		complete: setTimeout(function() {poll()}, 5000),
-		timeout: 2000
-	})
-	})();
+	
 	
 
 });
