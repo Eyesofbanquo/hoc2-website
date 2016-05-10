73fd306c98e002fe0b0964a14fd4f829249f2d14
Eyesofbanquo <mls103020@utdallas.edu>
2016-05-10 16:48:42 -0500

    the js should be fixed with the right images now

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,25 @@
 $(function(){
 	$(".rslides").responsiveSlides();
-	$("#liveId").html("scrub");
+	(function poll() {
+		$.ajax({
+		url: "https://api.twitch.tv/kraken/streams/bum1six3",
+		type: "GET",
+		success: function(data){
+			if (data["stream"] == null) {
+				$("#liveId").html("offline");
+			} else {
+				$("#liveId").html("live");
+				$("#liveId").click(function(){
+					window.location = "http://www.twitch.tv/bum1six3/embed";
+				});
+			}
+			console.log("polling");
+		},
+		dataType: "json",
+		complete: setTimeout(function() {poll()}, 5000),
+		timeout: 2000
+	})
+	})();
 });
 
 
