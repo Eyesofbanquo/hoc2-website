c1606887f462d92375cf30e6eee33c4b159ff86e
Eyesofbanquo <mls103020@utdallas.edu>
2016-05-10 16:50:28 -0500

    had to remove typo in myapp.rb

--- myapp.rb
+++ myapp.rb
@@ -4,15 +4,7 @@ require 'slim/include'
 require 'rest-client'
 
 #Check to see if the stream is online. Use this information to update the button on main page
-img class="pure-img" src="/img/houseofchaoslogo.png"
 isLive = (JSON.parse RestClient.get "https://api.twitch.tv/kraken/streams/bum1six3")["stream"]
-isLiveClass = "offline"
-if isLive === nil
-	isLiveClass = "offline"
-else
-	isLiveClass = "online"
-end
-
 isLiveClass = "offline"
 if isLive === nil
 	isLiveClass = "offline"
